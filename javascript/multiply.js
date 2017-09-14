b3dfb03bd8cc41487a419d3f70781aadf4cb6f4b
landonwjohnson <landonwjohnson@gmail.com>
2017-09-13 19:17:40 -0600

    multiply

---
+++ javascript/multiply.js
@@ -0,0 +1,10 @@
+// This function has to be called multiply and needs 
+// to take two numbers as arguments, and has to return 
+// the multiplication of the two arguments.
+
+
+function multiply(num1, num2) {
+    return num1 * num2;
+    }
+  
+multiply(5, 6);
